fddd77c8e609ea0251051a96f6457366fdccf3fa
zmdlw <giraffe0582@gmail.com>
2022-05-20 00:32:11 +0900

    Done!

--- main.py
+++ main.py
@@ -78,3 +78,10 @@ def test(test_dataloader, model, loss_fn):
     test_loss/=num_batches
     correct/=size
     print(f'Test Error: \n Accuracy: {(100*correct): 0.1f}%, Avg loss: {test_loss:>8f} \n')
+
+epochs = 10
+for t in range(epochs):
+    print(f'Epoch {t+1}\n ---------------------------')
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
